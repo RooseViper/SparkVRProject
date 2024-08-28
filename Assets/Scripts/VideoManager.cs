8cc6fc8f0590eff678d0e65a70f7db7f9dcc165b
RooseViper <roosemwandira@gmail.com>
2024-08-28 22:21:14 +0200

    Add: Added Active citizen scene

--- Assets/Scripts/VideoManager.cs
+++ Assets/Scripts/VideoManager.cs
@@ -9,17 +9,24 @@ using UnityEngine.Video;
 
 public class VideoManager : MonoBehaviour
 {
+    [SerializeField] private Transform panel;
+    [SerializeField] private Button expandButton;
     [SerializeField]private List<VideoInfo> loadedVideos;
     [SerializeField] private Image[] displayImages;
     private VideoPlayer videoPlayer;
+    private Vector3 expandPosition;
+    private bool isExpanded;
     private void Start()
     {
+        expandPosition = panel.localScale;
+        panel.localScale = Vector3.zero;
         videoPlayer = GetComponent<VideoPlayer>();
         for (var i = 0; i < loadedVideos.Count; i++)
         {
             displayImages[i].sprite = loadedVideos[i].thumbnail;
             displayImages[i].GetComponentInChildren<TextMeshProUGUI>().text = loadedVideos[i].videoName;
         }
+        expandButton.onClick.AddListener(ChangePanelSize);
         videoPlayer.Play();
     }
 
@@ -28,6 +35,25 @@ public class VideoManager : MonoBehaviour
         var vInfo = loadedVideos[index];
         videoPlayer.clip = vInfo.clip;
         videoPlayer.Play();
-        Debug.Log("Called Video" + index);
     }
+    /// <summary>
+    /// Expands or collapses the panel
+    /// </summary>
+    private void ChangePanelSize()
+    {
+        isExpanded =!isExpanded;
+        if (isExpanded)
+        {
+            LeanTween.scale(panel.gameObject, expandPosition, 0.5f).setEaseInOutSine().setOnComplete(ReachedEnd);
+            LeanTween.moveLocalX(expandButton.gameObject, 680f, 0.5f).setEaseInOutSine();
+        }
+        else
+        {
+            LeanTween.scale(panel.gameObject, Vector3.zero, 0.5f).setEaseInOutSine().setOnComplete(ReachedEnd);
+            LeanTween.moveLocalX(expandButton.gameObject, -556f, 0.5f).setEaseInOutSine();
+        }
+        expandButton.interactable = false;
+    }
+
+    private void ReachedEnd() => expandButton.interactable = true;
 }
