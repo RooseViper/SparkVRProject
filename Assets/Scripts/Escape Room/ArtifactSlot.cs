ca41b0d900201926ea7ef50e3207ea2ecaa1af7e
RooseViper <roosemwandira@gmail.com>
2024-12-05 16:14:43 +0200

    Add: Created new room

--- Assets/Scripts/Escape Room/ArtifactSlot.cs
+++ Assets/Scripts/Escape Room/ArtifactSlot.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
@@ -11,15 +12,36 @@ namespace Escape_Room
         private XRSocketInteractor xrSocketInteractor;
         private Item item;
         private GlassBox glassBox;
-        private AudioSource audioSource;
+        private AudioSource glassBoxAudioSource, audioSource;
         // Start is called before the first frame update
         private void Start()
         {
             xrSocketInteractor = GetComponent<XRSocketInteractor>();
             glassBox = GetComponentInParent<GlassBox>();
-            audioSource = glassBox.GetComponent<AudioSource>();
+            audioSource = GetComponent<AudioSource>();
+            glassBoxAudioSource = glassBox.GetComponent<AudioSource>();
             xrSocketInteractor.selectEntered.AddListener(SetArtifactPiece);
             xrSocketInteractor.selectExited.AddListener(RemoveArtifactPiece);
+        }
+        private void RemoveFromSocket()
+        {
+            var attachedInteractable = xrSocketInteractor.firstInteractableSelected;
+            if (attachedInteractable != null)
+            {
+                Escape_Room.Audio.AudioManager.Instance.Play("Object snap", audioSource);
+                // Force the socket to release the interactable
+                xrSocketInteractor.EndManualInteraction();
+                xrSocketInteractor.interactionManager.SelectExit(xrSocketInteractor, attachedInteractable);
+
+                // Optional: Adjust interactable's position or parent if needed
+                var interactableTransform = attachedInteractable.transform;
+                interactableTransform.SetParent(null); // Detach from socket
+                interactableTransform.position += Vector3.left * 0.1f; // Move it slightly upwards
+            }
+            else
+            {
+                Debug.Log("No interactable is currently attached to the socket.");
+            }
         }
         
         private void SetArtifactPiece(SelectEnterEventArgs selectEnterEventArgs)
@@ -30,6 +52,11 @@ namespace Escape_Room
                 item = attachedArtifact;
                 item.MakeKinematic();
             }
+
+            if (index != attachedArtifact.index)
+            {
+                RemoveFromSocket();
+            }
             var artifactSlots = transform.parent.GetComponentsInChildren<ArtifactSlot>();
             var allSlotsFilled = artifactSlots.All(slot => slot.HasPiece);
             if (allSlotsFilled)
@@ -39,7 +66,7 @@ namespace Escape_Room
                 {
                     var door = transform.parent.GetComponentInChildren<Door>();
                     door.Open();
-                    Escape_Room.Audio.AudioManager.Instance.Play("Glassbox Open", audioSource);
+                    Escape_Room.Audio.AudioManager.Instance.Play("Glassbox Open", glassBoxAudioSource);
                     glassBox.MakePieceInteractable();
                 }
             }
