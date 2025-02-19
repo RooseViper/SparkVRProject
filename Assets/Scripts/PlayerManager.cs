30a2ae50b11c7d7f513d88a6a267cf8d4cb633ee
RooseViper <roosemwandira@gmail.com>
2025-02-19 13:05:40 +0200

    Add: Added snap player coroutine

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
    public Transform playerRig;
-   [SerializeField] private Transform teleportTransform;
+   [SerializeField] private Transform teleportTransformLevel, teleportTransformStartScene;
    public static PlayerManager Instance => _instance;
    private static PlayerManager _instance;
 
@@ -14,8 +14,24 @@ public class PlayerManager : MonoBehaviour
    {
       _instance = this;
    }
+
+   private void Start()
+   {
+      StartCoroutine(SnapPlayerCoroutine());
+   }
+
+   /// <summary>
+   /// Snaps Player to starting room
+   /// </summary>
+   /// <returns></returns>
+   private IEnumerator SnapPlayerCoroutine()
+   {
+      yield return new WaitForEndOfFrame();
+      playerRig.SetLocalPositionAndRotation(teleportTransformStartScene.position, Quaternion.Euler(teleportTransformStartScene.eulerAngles));
+   }
+
    public void Teleport()
    {
-      playerRig.SetLocalPositionAndRotation(teleportTransform.position, Quaternion.Euler(teleportTransform.eulerAngles));
+      playerRig.SetLocalPositionAndRotation(teleportTransformLevel.position, Quaternion.Euler(teleportTransformLevel.eulerAngles));
    }
 }
