964c295c8ff0dd278d05518f79d8ab081da88d7b
RooseViper <roosemwandira@gmail.com>
2025-02-24 21:19:16 +0200

    Add: Added Shrink effect

--- Assets/Scripts/Shrinker.cs
+++ Assets/Scripts/Shrinker.cs
@@ -6,14 +6,24 @@ using UnityEngine;
 public class Shrinker : MonoBehaviour
 {
     private Canvas canvas;
-
+    private Vector3 defaultScale;
     private void Start()
     {
         canvas = GetComponent<Canvas>();
+        var canvasTransform = canvas.transform;
+        defaultScale = canvasTransform.localScale;
+        canvasTransform.localScale = Vector3.zero;
+        UnShrink();
     }
 
     // Start is called before the first frame update
-    public void Shrink()=>LeanTween.scale(gameObject, Vector3.zero, 0.25f).setEaseInOutSine().setOnComplete(OnEnd);
+    public void Shrink()=>LeanTween.scale(gameObject, Vector3.zero, 0.5f).setEaseInOutSine().setOnComplete(OnEnd);
+
+    public void UnShrink()
+    {
+        canvas.enabled = true;
+        LeanTween.scale(gameObject, defaultScale, 0.5f).setEaseInOutSine();
+    }
 
     private void OnEnd() => canvas.enabled = false;
 }
