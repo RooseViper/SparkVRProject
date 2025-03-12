c0c124f745f204509dc6ee7e36f70c06d9d68646
RooseViper <roosemwandira@gmail.com>
2025-03-12 11:56:01 +0200

    Add: Added example artifact item

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -29,6 +29,13 @@ public class GameManager : MonoBehaviour
         defaultPortableMenuCanvasSize = portableMenuCanvas.localScale;
         portableMenuCanvas.localScale = Vector3.zero;
         Application.targetFrameRate = 60;
+        foreach (var obj in FindObjectsOfType<GameObject>())
+        {
+            if (obj.layer == 20)
+            {
+                Debug.Log(obj.name);
+            }
+        }
         ftLightmaps.RefreshFull();
     }
     
