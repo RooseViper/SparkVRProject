b87ff09f57276913c6806fbcd9016156f5f2ffa9
RooseViper <roosemwandira@gmail.com>
2025-03-28 14:14:52 +0200

    Add: Added Old Man tackle

---
+++ Assets/Scripts/FootSteps.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Audio;
+using Micosmo.SensorToolkit;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class FootSteps : MonoBehaviour
+{
+   [SerializeField]private AudioClip[] concreteFootStepClips;
+   [SerializeField]private AudioClip[] metalFootStepClips;
+   [SerializeField]private AudioClip[] woodenFootStepClips;
+   [SerializeField]private AudioClip[] movementClips;
+   [SerializeField]private AudioSource audioSource;
+   [SerializeField]private LOSSensor floorSensor; //Supported with a Range sensor
+   private GameObject detectedFloor;
+
+   /// <summary>
+   /// Called in an animaton between keyframes
+   /// </summary>
+    public void PlayFootStep()
+    {
+        detectedFloor = floorSensor.GetNearestDetection();
+        if(detectedFloor == null)return;
+        if (detectedFloor.CompareTag("Concrete") || detectedFloor.CompareTag("Untagged"))
+        {
+            audioSource.PlayOneShot(concreteFootStepClips[Random.Range(0, concreteFootStepClips.Length)]);
+        }  
+        else if (detectedFloor.CompareTag("Metal"))
+        {
+            audioSource.PlayOneShot(metalFootStepClips[Random.Range(0, metalFootStepClips.Length)]);
+        }
+        else if (detectedFloor.CompareTag("Wood"))
+        {
+            audioSource.PlayOneShot(woodenFootStepClips[Random.Range(0, woodenFootStepClips.Length)]);
+        }
+    }
+
+   public void PlayMovementClip()=> audioSource.PlayOneShot(woodenFootStepClips[Random.Range(0, woodenFootStepClips.Length)]);
+
+   public void TriggerChase()
+   {
+       var oldManAi = GetComponentInParent<OldManAi>();
+       Debug.Log("11");
+       if(oldManAi == null)return;
+       Debug.Log("22");
+       oldManAi.Chase();
+   }
+}
--- Assets/Scripts/OldManAi.cs
+++ Assets/Scripts/OldManAi.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using BehaviorDesigner.Runtime;
+using Pathfinding;
 using UnityEngine;
 
 public class OldManAi : MonoBehaviour
 {
+    [SerializeField]private BehaviorTree chaseBehavior;
+    [SerializeField]private AudioSource chaseAudioSource;
+    private Animator animator;
+
+    private static readonly int IsWalking = Animator.StringToHash("IsWalking");
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        animator = transform.GetChild(0).GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LockDown()
     {
-        
+        animator.SetBool(IsWalking, true);
+        chaseAudioSource.Play();
+    }
+
+    public void Chase()
+    {
+        chaseBehavior.EnableBehavior();
     }
 }
